999d7b2e5e4f428839e947430caf14649f816bf6
gabo-hacStyle <gabo2023brazil@gmail.com>
2026-01-19 08:25:47 -0500

    feat: add sonido

--- data.js
+++ data.js
@@ -1,12 +1,14 @@
 export const availableInstruments = [
+ 
+   'Coro1',
+    'Coro2',
+    'Coro3',
+    'Coro4',
+    'Piano',
+    'Piano 2',
     'Guitarra 1',
     'Guitarra 2',
     'Bajo',
     'Batería',
-    'Piano',
-    'Piano 2',
-    'Coro1',
-    'Coro2',
-    'Coro3',
-    'Coro4'
+    'Sonido'
   ];
